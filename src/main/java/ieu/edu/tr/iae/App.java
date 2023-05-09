34f836b93b18701a803a1727b9abcf13715ad37c
bilalsarac <bilalcan1999@hotmail.com>
2023-05-09 16:09:45 +0300

    -

--- src/main/java/ieu/edu/tr/iae/App.java
+++ src/main/java/ieu/edu/tr/iae/App.java
@@ -4,15 +4,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 550, 500);
+        stage.setTitle("IAE");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/ieu/edu/tr/iae/controllers/AppController.java
+++ src/main/java/ieu/edu/tr/iae/controllers/AppController.java
@@ -1,14 +1,42 @@
 package ieu.edu.tr.iae.controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
 
 public class AppController {
     @FXML
-    private Label welcomeText;
+    private Button configButton;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button runButton;
+
+    @FXML
+    private MenuItem menuItemHelp;
+
+    @FXML
+    private TreeView<?> treeView;
+    @FXML
+    private void initialize(){
+        System.out.println("-************************************");
+
+
+
+        menuItemHelp.setOnAction((value) -> {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Help");
+            alert.setHeaderText("Help");
+            alert.setContentText("Welcome to IAE");
+            TextArea area = new TextArea(menuItemHelp.getText());
+            area.setText("Welcome to IAE");
+            area.setWrapText(true);
+            area.setEditable(false);
+
+            alert.getDialogPane().setExpandableContent(area);
+            alert.showAndWait();
+        });
     }
+
 }
